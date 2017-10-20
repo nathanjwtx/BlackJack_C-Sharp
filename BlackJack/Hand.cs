615e045e10e019018aa516b4c87951da4b458250
nathanjwtx <dynamonath@nathanjwtx.me>
2017-10-19 19:24:53 -0500

    + Actually added Hand.cs this time
    Signed-off-by: nathanjwtx <dynamonath@nathanjwtx.me>

---
+++ BlackJack/Hand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlackJack
+{
+    class Hand
+    {
+        private readonly CardDeck _deck;
+        private int HandValue { get; set; }
+        public List<string> PlayerHand { get; set; }
+        
+        public Hand (CardDeck _deck)
+        {
+            this._deck = _deck;
+            this.PlayerHand = new List<string>();
+        }
+
+        public void Deal()
+        {
+            PlayerHand.Add(this._deck.DealCard());
+        }
+
+        public void GetHand()
+        {
+            foreach (var card in PlayerHand)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+        public void SetHandValue()
+        {
+            var score = 0;
+            foreach (var card in PlayerHand)
+            {
+                if (card.Length <= 0) continue;
+                switch (card[0])
+                {
+                    case 'A':
+                        Console.WriteLine("Ace is 1 or 11?");
+                        score += Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 'K':
+                        score += 10;
+                        break;
+                    case 'Q':
+                        score += 10;
+                        break;
+                    case 'J':
+                        score += 10;
+                        break;
+                    case '1':
+                        score += 10;
+                        break;
+                    default:
+                        var x = char.GetNumericValue(card[0]);
+                        score += Convert.ToInt32(x);
+                        break;
+                }
+            }
+            this.HandValue = score;
+        }
+
+        public int GetHandValue()
+        {
+            return this.HandValue;
+        }
+    }
+}
